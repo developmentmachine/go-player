8325bc369d774c54db2aa6fc1177d2f6ec9c7a07
bountyhunter <bountyhunter@live.com>
2021-12-16 23:06:10 +0800

    update

--- application.go
+++ application.go
@@ -10,42 +10,44 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/playing", playing)
+	http.HandleFunc("/playing/", playing)
 	if err := http.ListenAndServe(":80", nil); err != nil {
 		log.Fatalln(err)
 	}
 }
 
 func playing(resp http.ResponseWriter, req *http.Request) {
-	file, err := os.Open("d:\\test.mp4")
-	if err != nil {
+	if file, err := os.Open("d:\\test.mp4"); err != nil {
 		resp.WriteHeader(500)
 		return
-	}
-	stat, err := file.Stat()
-	if err != nil {
-		resp.WriteHeader(500)
-		return
-	}
-	var minRange = "0"
-	var maxRange = strconv.Itoa(int(stat.Size() - 1))
-	if req.Header.Get("Range") != "" && strings.Contains(req.Header.Get("Range"), "-") {
-		r := strings.Split(strings.ReplaceAll(req.Header.Get("Range"), "bytes=", ""), "-")
-		minRange = r[0]
-		if len(r) == 2 && r[1] != "" {
-			maxRange = r[1]
+	} else {
+		defer file.Close()
+		if stat, err := file.Stat(); err != nil {
+			resp.WriteHeader(500)
+			return
+		} else {
+			var minRange = "0"
+			var maxRange = strconv.Itoa(int(stat.Size() - 1))
+			if req.Header.Get("Range") != "" && strings.Contains(req.Header.Get("Range"), "-") {
+				r := strings.Split(strings.ReplaceAll(req.Header.Get("Range"), "bytes=", ""), "-")
+				minRange = r[0]
+				if len(r) == 2 && r[1] != "" {
+					maxRange = r[1]
+				}
+				resp.Header().Add("Content-Range", strings.Join([]string{"bytes ", r[0], "-", maxRange, "/", strconv.Itoa(int(stat.Size()))}, ""))
+			}
+			resp.Header().Add("Content-Length", strconv.Itoa(int(stat.Size())))
+			resp.Header().Add("Content-Type", "video/mp4")
+			resp.WriteHeader(206)
+			seek, _ := strconv.ParseInt(minRange, 10, 64)
+			file.Seek(seek, io.SeekStart)
+			buffer := make([]byte, 204800)
+			for {
+				if read, err := file.Read(buffer); read != 0 && err != io.EOF {
+					resp.Write(buffer)
+				}
+			}
 		}
-		resp.Header().Add("Content-Range", strings.Join([]string{"bytes ", r[0], "-", maxRange, "/", strconv.Itoa(int(stat.Size()))}, ""))
 	}
-	resp.Header().Add("Content-Length", strconv.Itoa(int(stat.Size())))
-	resp.Header().Add("Content-Type", "video/mp4")
-	resp.WriteHeader(206)
-	seek, _ := strconv.ParseInt(minRange, 10, 64)
-	file.Seek(seek, io.SeekStart)
-	buffer := make([]byte, 204800)
-	for {
-		if read, err := file.Read(buffer); read != 0 && err != io.EOF {
-			resp.Write(buffer)
-		}
-	}
+
 }
